810f0230004edf884c8e1e459d4249aa7c693331
Кирилл <kkirillcore@gmail.com>
2020-06-21 21:58:55 +0300

    добавляет проект на гитхаб

---
+++ build/js/slider.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function () {
+  $('.concert-halls__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: false,
+  });
+});
---
+++ source/js/mask.js
@@ -0,0 +1,5 @@
+'use strict';
+
+$(function () {
+  $('#discuss-phone').mask('+7(999) 999-99-99');
+});
