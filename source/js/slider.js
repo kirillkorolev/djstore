f9266b5102c88b1ec2b728c414e45911da97049d
Кирилл <kkirillcore@gmail.com>
2020-06-24 17:34:55 +0300

    правки после замечаний

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,7 +1,16 @@
 'use strict';
 
 $(document).ready(function () {
+  $('.concert-halls__slideshow').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.concert-halls__slider',
+  });
+
   $('.concert-halls__slider').slick({
+    asNavFor: '.concert-halls__slideshow',
     slidesToShow: 1,
     slidesToScroll: 1,
     variableWidth: true,
